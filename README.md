4a0ee52305b8ddd2e2c961f0e5b55d4532bef659
Rhythm Verma <51092166+ritmojs@users.noreply.github.com>
2020-06-26 01:06:16 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,54 @@
+<p align="center">
+  <a>
+    <img src="./fontify.png" width = "1400px"/>
+  </a>
+</p>
+<h1 align="center">
+  fontify
+</h1>
+
+ 
+</p>
+
+## What is Fontify?
+* fontify allows user to feel the real beauty of cursive font.
+
+
+
+# How to download Fontify
+
+* git repo is public you can clone it from here (for developers only)
+* or from chrome webstore.
+
+
+
+# ScreenShots
+<img src="./githubss.png">
+<img src="./udacityss.png">
+<img src="./screenshot.png">
+
+
+
+# How to Install as Chrome Extension?
+ * * You can download this from the Chrome Store:
+
+* The fontify Chrome Store Page
+* To install from source added as an "unpacked extension":
+* download the zip file from releases
+* unzip the file
+* visit chrome://extensions
+* switch on Developer mode
+* Click Load Unpacked
+* choose the extension folder (the one with the manifest.json in it)
+
+
+
+# ThankYou 
+<p align="center">
+<img src="./logo.png" width="200" height ="200">
+</p>
+
+<p align="center">
+
+## © fotify 2020
+</p>
---
+++ content.js
@@ -0,0 +1,47 @@
+
+  let paragraphs = document.getElementsByTagName("body");
+    let atags=document.getElementsByTagName("a");
+    let h6tags=document.getElementsByTagName("h1");
+    let h1tags=document.getElementsByTagName("h2");
+    let h2tags=document.getElementsByTagName("h3");
+    let h3tags=document.getElementsByTagName("h4");
+    let h4tags=document.getElementsByTagName("h5");
+    let h5tags=document.getElementsByTagName("h6");
+    let ptags=document.getElementsByTagName("p");
+	for(elt of paragraphs)
+	{
+		elt.style["font-family"] = ' cursive';
+    }
+    for(elt of h1tags)
+	{
+		elt.style["font-family"] = ' cursive';
+    }
+    for(elt of h2tags)
+	{
+		elt.style["font-family"] = ' cursive';
+    }
+    for(elt of h3tags)
+	{
+		elt.style["font-family"] = ' cursive';
+    }
+    for(elt of h4tags)
+	{
+		elt.style["font-family"] = ' cursive';
+    }
+    for(elt of h5tags)
+	{
+		elt.style["font-family"] = ' cursive';
+    }
+    for(elt of h6tags)
+	{
+		elt.style["font-family"] = ' cursive';
+    }
+    for(elt of atags)
+	{
+		elt.style["font-family"] = ' cursive';
+    }
+    for(elt of ptags)
+	{
+        elt.style["font-family"] = ' cursive';
+	}
+
